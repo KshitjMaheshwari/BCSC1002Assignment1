9c3878a96948def9c00bfcb34e8b6c1e98860cf0
Lenovo <kshitij.maheshwari_cs19@gla.ac.in>
2020-08-27 11:52:09 +0530

    create equals() and hashCode() method for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private String bookAuthor;
@@ -97,5 +99,20 @@ public class Book {
                 ", bookAuthor='" + bookAuthor + '\'' +
                 ", bookISBNNumber='" + bookISBNNumber + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(bookAuthor, book.bookAuthor) &&
+                Objects.equals(bookISBNNumber, book.bookISBNNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, bookAuthor, bookISBNNumber);
     }
 }
