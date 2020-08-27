acb5d4a85ea03b1db14e9eb29360b12e061701c9
Lenovo <kshitij.maheshwari_cs19@gla.ac.in>
2020-08-27 11:46:45 +0530

    create getter and setter for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,65 @@ public class Book {
     private String bookName;
     private String bookAuthor;
     private String bookISBNNumber;
+
+    /**
+     * This method returns the name of the book.
+     *
+     * @return The name of the book.
+     */
+    public String getBookName() {
+
+        return bookName;
+    }
+
+    /**
+     * This method sets the name of the book.
+     *
+     * @param bookName The name of the book.
+     */
+
+    public void setBookName(String bookName) {
+
+        this.bookName = bookName;
+    }
+
+    /**
+     * This method returns the name of the book.
+     *
+     * @return The author of the book.
+     */
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    /**
+     * This method sets the name of the book.
+     *
+     * @param bookAuthor The name of the book.
+     */
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    /**
+     * This method returns the name of the book.
+     *
+     * @return The ISBN number of the book.
+     */
+
+    public String getBookISBNNumber() {
+        return bookISBNNumber;
+    }
+
+    /**
+     * This method sets the name of the book.
+     *
+     * @param bookISBNNumber The name of the book.
+     */
+
+    public void setBookISBNNumber(String bookISBNNumber) {
+        this.bookISBNNumber = bookISBNNumber;
+    }
+
 }
