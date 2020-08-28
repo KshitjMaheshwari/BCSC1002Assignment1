bc4ce681e0668d3b15e94ca704222cc9d8ba1b22
Lenovo <kshitij.maheshwari_cs19@gla.ac.in>
2020-08-28 20:52:08 +0530

    create constructors for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,7 +7,19 @@
 package definitions;
 
 public class Library {
+    private final static int CAPACITY_OF_BOOKS_STORED_IN_LIBRARY = 120;
     private Book[] currentlyAvailableBooks;
+
+    public Library() {
+        currentlyAvailableBooks = new Book[CAPACITY_OF_BOOKS_STORED_IN_LIBRARY];
+        for (int index = 0; index < CAPACITY_OF_BOOKS_STORED_IN_LIBRARY; index++) {
+            currentlyAvailableBooks[index] = new Book();
+        }
+    }
+
+    public Library(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
 
     public Book[] getCurrentlyAvailableBooks() {
         return currentlyAvailableBooks.clone();
